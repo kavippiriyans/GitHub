2013c6d81c45f13f9b5b206600bb0940bcb2384c
Kavippiriyan <138883580+Kavippiriyan@users.noreply.github.com>
2023-07-08 16:52:06 +0530

    Commit5
    Commit5

--- GitHub/src/main/java/GitHub/Practice1.java
+++ GitHub/src/main/java/GitHub/Practice1.java
@@ -5,7 +5,7 @@ public class Practice1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Good Evening");
-System.out.println("hi");
+		System.out.println("hi");
 	}
 
 }
