2386c91b0cf554ee97ff0d2ba96adc9f024c6078
Kavippiriyan <138883580+Kavippiriyan@users.noreply.github.com>
2023-07-12 16:56:48 +0530

    commit

--- GitHub/src/main/java/GitHub1/demo1.java
+++ GitHub/src/main/java/GitHub1/demo1.java
@@ -3,9 +3,9 @@ package GitHub1;
 public class demo1 {
 
 	public static void main(String[] args) {
-		int x=10;
-		int y=20;
-		int z=x+y;
+		int a1=10;
+		int a2=20;
+		int z=a1+a2;
 		System.out.println(z);
 	}
 
